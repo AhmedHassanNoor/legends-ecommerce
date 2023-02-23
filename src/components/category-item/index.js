0890301c7d42bb04b09e1bb8a00ca8ed45577487
AhmedHassanNoor <ahmed.hassannoor123@gmail.com>
2023-02-23 09:57:09 +0200

    cleaned up the home directory component

--- src/components/category-item/index.js
+++ src/components/category-item/index.js
@@ -1,8 +1,12 @@
 import { CategoryContainer, CategoryBody, BackgroundImage } from "./styles";
+import { useNavigate } from "react-router-dom";
 
-const CategoryItem = ({ title, imageUrl }) => {
+const CategoryItem = ({ title, imageUrl, route }) => {
+  const navigate = useNavigate();
+
+  const onNavigateHandler = () => navigate(route);
   return (
-    <CategoryContainer>
+    <CategoryContainer onClick={onNavigateHandler}>
       <BackgroundImage imageUrl={imageUrl} />
       <CategoryBody>
         <h2>{title}</h2>
--- src/components/directory/index.js
+++ src/components/directory/index.js
@@ -2,11 +2,44 @@ import { DirectoryContainer } from "./styles";
 
 import CategoryItem from "../category-item";
 
-const CategoryList = ({ categories }) => {
+const categories = [
+  {
+    id: 1,
+    title: "hats",
+    imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+    route: "shop/hats",
+  },
+  {
+    id: 2,
+    title: "jackets",
+    imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+    route: "shop/jackets",
+  },
+  {
+    id: 3,
+    title: "sneakers",
+    imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+    route: "shop/sneakers",
+  },
+  {
+    id: 4,
+    title: "womens",
+    imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+    route: "shop/womens",
+  },
+  {
+    id: 5,
+    title: "mens",
+    imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+    route: "shop/mens",
+  },
+];
+
+const CategoryList = () => {
   return (
     <DirectoryContainer>
-      {(categories || []).map(({ title, id, imageUrl }) => (
-        <CategoryItem key={id} title={title} imageUrl={imageUrl} />
+      {(categories || []).map(({ title, id, imageUrl, route }) => (
+        <CategoryItem key={id} route={route} title={title} imageUrl={imageUrl} />
       ))}
     </DirectoryContainer>
   );
