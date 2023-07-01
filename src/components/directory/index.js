78c810a9e215c771c9ccf938d662e7d2d5a72db1
AhmedHassanNoor <ahmed.hassannoor123@gmail.com>
2023-07-01 09:35:43 +0200

    Added loading spinner to the homepage

--- src/components/directory/index.js
+++ src/components/directory/index.js
@@ -1,22 +1,32 @@
 import { DirectoryContainer } from "./styles";
 
 import { useSelector } from "react-redux";
-import { selectCategoriesMap } from "../../store/categories/selector";
+import { selectCategoriesIsLoading, selectCategoriesMap } from "../../store/categories/selector";
 import ProductCard from "../product-card";
+import Spinner from "../spinner";
+import { Fragment } from "react";
 
 const DirectoryList = () => {
   const categories = useSelector(selectCategoriesMap);
+  const isLoading = useSelector(selectCategoriesIsLoading);
   const products = Object.keys(categories).reduce((acc, key) => {
     acc.push(...categories[key]);
     return acc.sort(() => 0.5 - Math.random());
   }, []);
 
   return (
-    <DirectoryContainer>
-      {products.map((product) => (
-        <ProductCard key={product.id} product={product} />
-      ))}
-    </DirectoryContainer>
+    <Fragment>
+      {isLoading || !products.length ? (
+        <Spinner />
+      ) : (
+        <DirectoryContainer>
+          {products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </DirectoryContainer>
+      )}
+      ;
+    </Fragment>
   );
 };
 
